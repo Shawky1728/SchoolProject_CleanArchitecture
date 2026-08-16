1ff3465f1f610d65177634cba7be3c44f14985af
Mahmoud shawky <121881335+Shawky1728@users.noreply.github.com>
2026-08-16 14:20:45 +0300

    Refactor API: add Students, Swagger, DI; cleanup samples

---
+++ SchoolProject.Api/Controllers/StudentsController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SchoolProject.Core.Features.Students.Queries.GetStudents;
+
+namespace SchoolProject.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public StudentsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllStudents(CancellationToken cancellationToken)
+        {
+            var query = new GetStudentsQuery();
+            var students = await _mediator.Send(query, cancellationToken);
+            return Ok(students);
+        }
+    }
+}
--- SchoolProject.Api/Program.cs
+++ SchoolProject.Api/Program.cs
@@ -1,6 +1,9 @@
 
 using Microsoft.EntityFrameworkCore;
+using SchoolProject.Core;
+using SchoolProject.Infrastructure;
 using SchoolProject.Infrastructure.Data;
+using SchoolProject.Service;
 
 namespace SchoolProject.Api
 {
@@ -16,16 +19,34 @@ namespace SchoolProject.Api
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            // register Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             // Add Connection String
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            #region Dependency Injection
+
+            // Add Infrastructure Dependencies
+            builder.Services.AddInfrastructureDependencies();
+
+            // Add Service Dependencies
+            builder.Services.AddServiceDependencies();
+
+            // Add Core Dependencies
+            builder.Services.AddCoreDependencies();
+
+            #endregion
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
