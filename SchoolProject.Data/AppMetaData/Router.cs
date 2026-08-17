2cb639c6171a503cfd874b0361a56c0cf93dd3bd
Mahmoud shawky <121881335+Shawky1728@users.noreply.github.com>
2026-08-17 15:12:29 +0300

    Add Router class with API route constants

---
+++ SchoolProject.Data/AppMetaData/Router.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace SchoolProject.Data.AppMetaData
+{
+    public static class Router
+    {
+        public const string Root = "api";
+
+        public static class Students
+        {
+            private const string Base = Root + "/students";
+            public const string GetAll = Base;
+            public const string GetById = Base + "/{id}";
+        }
+    }
+}
